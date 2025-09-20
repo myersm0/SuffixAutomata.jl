89865611c3d37c255f58809c7253c8365f0fe800
Michael Myers <michael.myers@wustl.edu>
2025-09-20 15:43:44 -0500

    cleaned up tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,52 +14,53 @@ using SuffixAutomata
 
 	@test substring_count(a) == 12
 
-	@test findall(collect("ab"), a) == [1, 3]
-	@test findall(collect("b"), a) == [2, 4]
-	@test findall(collect("abc"), a) == [3]
-	@test findall(collect("a"), a) == [1, 3]
-	@test findall(collect("c"), a) == [5]
-	@test findall(collect("d"), a) == Int[]
+	@test findall("ab", a) == [1, 3]
+	@test findall("b", a) == [2, 4]
+	@test findall("abc", a) == [3]
+	@test findall("a", a) == [1, 3]
+	@test findall("c", a) == [5]
+	@test findall("d", a) == Int[]
 
-	sub, pos = lcs(a, collect("zzabcy"))
+	sub, pos = lcs(a, "zzabcy")
 	@test String(sub) == "abc"
 	@test pos == 3  # Julia indices are 1-based
 
 	# empty automaton
-	b = SuffixAutomaton()
-	@test isempty(b)
-	@test !occursin("x", b)
-	@test substring_count(b) == 0
-	@test findall("x", b) == Int[]
+	a = SuffixAutomaton()
+	@test isempty(a)
+	@test !occursin("x", a)
+	@test substring_count(a) == 0
+	@test findall("x", a) == Int[]
 
 	# single character
-	c = SuffixAutomaton()
-	append!(c, "a")
-	@test occursin("a", c)
-	@test !occursin("b", c)
-	@test substring_count(c) == 1
-	 @test findall(collect("a"), c) == [1]
+	a = SuffixAutomaton()
+	append!(a, "a")
+	@test occursin("a", a)
+	@test !occursin("b", a)
+	@test substring_count(a) == 1
+	@test findall(collect("a"), a) == [1]
 
 	# repeated character
-	d = SuffixAutomaton()
-	append!(d, "aaaa")
-	@test occursin("aaa", d)
-	 @test findall(collect("aa"), d) == [1, 2, 3]
-	@test substring_count(d) == 4  # "a", "aa", "aaa", "aaaa"
+	a = SuffixAutomaton()
+	append!(a, "aaaa")
+	@test occursin("aaa", a)
+	@test occursin(collect("aaa"), a)
+	@test findall("aa", a) == findall(collect("aa"), a) == [1, 2, 3]
+	@test substring_count(a) == 4  # "a", "aa", "aaa", "aaaa"
 
-	e = SuffixAutomaton{Int}()
-	append!(e, [1, 2, 1, 3])
+	a = SuffixAutomaton{Int}()
+	append!(a, [1, 2, 1, 3])
 
 	# membership
-	@test occursin([1,2], e)
-	@test occursin([2,1], e)
-	@test !occursin([2,3], e)
+	@test occursin([1,2], a)
+	@test occursin([2,1], a)
+	@test !occursin([2,3], a)
 
 	# occurrences
-	@test findall([1], e) == [1, 3]
-	@test findall([1,3], e) == [3]
+	@test findall([1], a) == [1, 3]
+	@test findall([1,3], a) == [3]
 
 	# distinct substrings
-	@test substring_count(e) == 9
+	@test substring_count(a) == 9
 end
 
