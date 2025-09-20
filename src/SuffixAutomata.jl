9c184303582d70c668a4cfca7451f61c1113041e
Michael Myers <michael.myers@wustl.edu>
2025-09-20 12:37:15 -0500

    good working version

--- src/SuffixAutomata.jl
+++ src/SuffixAutomata.jl
@@ -4,15 +4,15 @@ module SuffixAutomata
 export SuffixAutomaton, longest_common_substring, substring_count, get_all_substrings
 
 mutable struct State{T}
-	transitions::Dict{T, State{T}}
-	suffix_link::Union{State{T}, Nothing}
+	transitions::Dict{T,State{T}}
+	suffix_link::Union{State{T},Nothing}
 	length::Int
 	is_terminal::Bool
 	first_occurrence::Int
 end
 
 State{T}(length::Int = 0, first_pos::Int = 0) where {T} =
-	State{T}(Dict{T, State{T}}(), nothing, length, false, first_pos)
+	State{T}(Dict{T,State{T}}(), nothing, length, false, first_pos)
 
 mutable struct SuffixAutomaton{T}
 	root::State{T}
@@ -28,7 +28,7 @@ end
 
 SuffixAutomaton() = SuffixAutomaton{Char}()
 
-function clone_state(state::State{T}, new_length::Int) where {T}
+function clone(state::State{T}, new_length::Int) where {T}
 	new_state = State{T}(new_length, state.first_occurrence)
 	new_state.transitions = copy(state.transitions)
 	new_state.suffix_link = state.suffix_link
@@ -39,42 +39,34 @@ end
 function Base.push!(automaton::SuffixAutomaton{T}, symbol::T) where {T}
 	position = length(automaton.text)
 	push!(automaton.text, symbol)
-	
 	new_state = State{T}(automaton.last.length + 1, position)
 	push!(automaton.states, new_state)
-	
 	current = automaton.last
-	
 	# add transitions to the new state from all states on the suffix path
 	while current !== nothing && !haskey(current.transitions, symbol)
 		current.transitions[symbol] = new_state
 		current = current.suffix_link
 	end
-	
 	if current === nothing
 		# we've reached the root via suffix links
 		new_state.suffix_link = automaton.root
 	else
 		next_state = current.transitions[symbol]
-		
 		if current.length + 1 == next_state.length
 			# the transition is solid (goes to the right place)
 			new_state.suffix_link = next_state
 		else
 			# we need to split the state
-			clone = clone_state(next_state, current.length + 1)
+			clone = clone(next_state, current.length + 1)
 			push!(automaton.states, clone)
-			
 			next_state.suffix_link = clone
 			new_state.suffix_link = clone
-			
 			while current !== nothing && current.transitions[symbol] === next_state
 				current.transitions[symbol] = clone
 				current = current.suffix_link
 			end
 		end
 	end
-	
 	automaton.last = new_state
 	return automaton
 end
@@ -116,26 +108,19 @@ end
 
 function Base.findall(pattern, automaton::SuffixAutomaton{T}) where {T}
 	isempty(pattern) && return collect(1:length(automaton.text)+1)
+
 	state = find_state(automaton, pattern)
 	state === nothing && return Int[]
+
+	patlen = length(pattern)
 	positions = Int[]
-	pattern_length = length(pattern)
-	text_length = length(automaton.text)
-	for i in 1:(text_length - pattern_length + 1)
-		match = true
-		for j in 1:pattern_length
-			if automaton.text[i + j - 1] != pattern[j]
-				match = false
-				break
-			end
-		end
-		if match
+	for i in 1:(length(automaton.text) - patlen + 1)
+		if automaton.text[i:i+patlen-1] == pattern
 			push!(positions, i)
 		end
 	end
 	return positions
 end
-
 
 function longest_common_substring(
 		automaton1::SuffixAutomaton{T}, sequence2
@@ -205,19 +190,16 @@ end
 
 function get_all_substrings(automaton::SuffixAutomaton{T}) where {T}
 	substrings = Vector{Vector{T}}()
-	
 	function dfs(state::State{T}, path::Vector{T})
 		if state !== automaton.root && length(path) > 0
 			push!(substrings, copy(path))
 		end
-		
 		for (symbol, next_state) in state.transitions
 			push!(path, symbol)
 			dfs(next_state, path)
 			pop!(path)
 		end
 	end
-	
 	dfs(automaton.root, T[])
 	return substrings
 end
@@ -258,7 +240,4 @@ function Base.show(io::IO, ::MIME"text/plain", automaton::SuffixAutomaton{T}) wh
 	end
 end
 
-
 end
-
-
